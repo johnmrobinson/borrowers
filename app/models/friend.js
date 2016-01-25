9e6c185b0161b3547c393d652bbd7dc2b3523c66
John Robinson <john.robinson@wellzesta.com>
2016-01-25 10:53:39 -0500

    cleaned models/friends.js

--- app/models/friend.js
+++ app/models/friend.js
@@ -5,7 +5,5 @@ export default DS.Model.extend({
   lastName: DS.attr('string'),
   email: DS.attr('string'),
   twitter: DS.attr('string'),
-  totalArticles: DS.attr('number'),
-  buster: DS.attr('string'),
-  brown: DS.attr('string')
+  totalArticles: DS.attr('number')
 });
